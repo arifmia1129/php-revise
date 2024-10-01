c636b3dac4b4199d763d0c17cfb66d57952af105
Md. Arif <arifmia1129@gmail.com>
2024-10-01 11:48:39 +0600

    Create index.php
    Echo and comment

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello, php</h1>
+
+    <?php 
+    // I am revising php programming again
+    $food = "Pizza";
+    echo "I love ".$food;
+
+    /* 
+    1. I love Pizza
+    2. I love Burger
+    */
+    ?>
+</body>
+</html>
