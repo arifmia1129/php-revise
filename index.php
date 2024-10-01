20fd9f4705b954c54084b9fb76463b75d4d03cb0
Md. Arif <arifmia1129@gmail.com>
2024-10-01 12:40:29 +0600

    Number operation: Pi, Max, Min, Random, Round, Absulate etc

--- index.php
+++ index.php
@@ -9,76 +9,92 @@
     <h1>Hello, php</h1>
 
     <?php 
-    // I am revising php programming again
-    $food = "Pizza";
-    echo "I love ".$food . '<br>';
+    // // I am revising php programming again
+    // $food = "Pizza";
+    // echo "I love ".$food . '<br>';
 
-    /* 
-    1. I love Pizza
-    2. I love Burger
-    */
+    // /* 
+    // 1. I love Pizza
+    // 2. I love Burger
+    // */
 
-    // Variables
-    $num1 = 10;
-    $num2 = 20;
+    // // Variables
+    // $num1 = 10;
+    // $num2 = 20;
 
-    $sum = $num1 + $num2;
+    // $sum = $num1 + $num2;
 
-    echo 'The sum of '. $num1 . ' and ' . $num2 . ' is ' . $sum . '<br>';
+    // echo 'The sum of '. $num1 . ' and ' . $num2 . ' is ' . $sum . '<br>';
 
 
-    //Echo and Print
-    echo $num1 . '<br>';
+    // //Echo and Print
+    // echo $num1 . '<br>';
     
-    $newNum = print($num1);
+    // $newNum = print($num1);
 
-    echo $newNum;
+    // echo $newNum;
 
-    '<br>';
+    // '<br>';
 
-    // Data types
+    // // Data types
 
-    $name = 'Ariba';
-    $age = 20;
-    $isMarried = false;
-    $height = 5.5;
-    $gadgets = array('laptop', 'mobile', 'tv');
+    // $name = 'Ariba';
+    // $age = 20;
+    // $isMarried = false;
+    // $height = 5.5;
+    // $gadgets = array('laptop', 'mobile', 'tv');
 
-    var_dump($gadgets);
+    // var_dump($gadgets);
 
-    // Object with Class
-    class Phone {
-        var $model;
+    // // Object with Class
+    // class Phone {
+    //     var $model;
 
-        function phoneModel($number) {
-            global $model;
+    //     function phoneModel($number) {
+    //         global $model;
 
-            $model = $number;
+    //         $model = $number;
 
-            echo '<br>'.'The phone model is: '. $model;
-        }
-    }
+    //         echo '<br>'.'The phone model is: '. $model;
+    //     }
+    // }
 
-    $apple = new Phone();
+    // $apple = new Phone();
 
-    $apple -> phoneModel('iPhone 11');
+    // $apple -> phoneModel('iPhone 11');
 
-    $samsung = new Phone();
+    // $samsung = new Phone();
 
-    $samsung->phoneModel('Samsung Galaxy');
+    // $samsung->phoneModel('Samsung Galaxy');
 
-    //PHP string
+    // //PHP string
 
+    // echo '<br>';
+    // echo strlen('Hello World');
+    // echo '<br>';
+    // echo str_word_count('Hello World');
+    // echo '<br>';
+    // echo strrev('Hello World');
+    // echo '<br>';
+    // echo strpos('Hello World', 'W');
+    // echo '<br>';
+    // echo str_replace('World', 'PHP', 'Hello World');
+
+    //Number operation in PHP
+
+    echo (pi());
     echo '<br>';
-    echo strlen('Hello World');
+    echo (min(50, 12, 1, 2, 40, 5));
     echo '<br>';
-    echo str_word_count('Hello World');
+    echo (max(10, 159, 170, 1));
     echo '<br>';
-    echo strrev('Hello World');
+    echo (abs(-16));
     echo '<br>';
-    echo strpos('Hello World', 'W');
-    echo '<br>';
-    echo str_replace('World', 'PHP', 'Hello World');
+    echo (sqrt(64));
+    echo ('<br>');
+    echo (round(4.4));
+    echo ('<br>');
+    echo (rand(1, 10));
 
     ?>
 </body>
